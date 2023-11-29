618e56252d4a1b37966bdff71e0973af304fef86
jsjdndjndmsn <152083411+greensleep1646@users.noreply.github.com>
2023-11-29 19:59:37 +0300

    Update 1646curse.py

--- 1646curse.py
+++ 1646curse.py
@@ -1,36 +1,6 @@
-print("""Wifi Görüntüleyici
-                        --
-""")
-import time
-while True:
-    import subprocess
-    print("Sistem analiz ediliyor")
-    import time
-    time.sleep(1)
-    print("Bulunan Wifiler: ")
-    veri = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
-    sistemler = [i.split(":")[1][1:-1] for i in veri if "All User Profile" in i]
-    for i in sistemler:
-        sonuç = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split(
-            '\n')
-        sonuç = [b.split(":")[1][1:-1] for b in sonuç if "Key Content" in b]
-        try:
-            print(" \\{:<30}| Şifre:  {:<}".format(i, sonuç[0]))
-        except IndexError:
-            print(" \\{:<30}| Şifre:  {:<}".format(i, ""))
-    exe = int(input("\n \n \n1'e basarak yeniden sistemi analiz edebilirsiniz \n2'ye basarak çıkış yapabilirsiniz "))
-    if (exe == 1):
-        print("")
-        import time
-        time.sleep(1)
-[ICODE][ICODE][ICODE]
-[/ICODE][/ICODE][/ICODE]
+import pywhatkit as kit
 
-    elif (exe == 2):
-        print("")
-        import time
-        time.sleep(1)
-        break
-        quit()
-    else:[ICODE][/ICODE]
-        print("Bir hata yaptınız lütfen tekrar deneyin")
+try:
+    kit.sendwhatmsg("+90**********", "BU BİR OTOMATİK MESAJDIR", 19,40)
+except Exception as e:
+    print("Hata oluştu:", str(e))
